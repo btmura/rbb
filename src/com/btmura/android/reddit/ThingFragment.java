497150e291c5e888dfe5f219a664400a444ca082
Brian Muramatsu <btmura@gmail.com>
2012-01-21 23:29:19 -0800

    Fix Lifecycle for ThingFragment WebView

--- src/com/btmura/android/reddit/ThingFragment.java
+++ src/com/btmura/android/reddit/ThingFragment.java
@@ -12,6 +12,7 @@ import android.app.Fragment;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -33,14 +34,15 @@ import com.google.gson.stream.JsonReader;
 public class ThingFragment extends Fragment {
 
 	private static final String TAG = "ThingFragment";
+	
+	private static final String STATE_URL = "url";
 
 	private ThingHolder thingHolder;
 
 	private ProgressBar progress;
 	private WebView webView;
-	private Menu menu;
 
-	private LoadUrlTask task;
+	private ResolveUrlTask task;
 	private String url;
 
 	public static ThingFragment newInstance() {
@@ -62,70 +64,61 @@ public class ThingFragment extends Fragment {
 		setupWebView(webView);
 		return view;
 	}
-
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-		setHasOptionsMenu(true);
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-		webView.onResume();
-	}
-
-	@Override
-	public void onStart() {
-		super.onStart();
-		task = new LoadUrlTask();
-		task.execute();
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-		webView.onPause();
-	}
-
-	@Override
-	public void onStop() {
-		super.onStop();
-		if (task != null) {
-			task.cancel(true);
-			task = null;
-		}
-	}
-
+	
 	private void setupWebView(WebView webView) {
-		webView.setWebViewClient(new ThingWebClient());
 		webView.getSettings().setBuiltInZoomControls(true);
 		webView.getSettings().setDisplayZoomControls(false);
 		webView.getSettings().setJavaScriptEnabled(true);
 		webView.getSettings().setLoadWithOverviewMode(true);
 		webView.getSettings().setSupportZoom(true);
-		webView.getSettings().setUseWideViewPort(true);
+		webView.getSettings().setUseWideViewPort(true);		
+		webView.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				super.onPageStarted(view, url, favicon);
+				progress.setVisibility(View.VISIBLE);
+			}
+			
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				super.onPageFinished(view, url);
+				progress.setVisibility(View.GONE);
+			}
+		});
 	}
-
-	class ThingWebClient extends WebViewClient {
-		@Override
-		public void onPageFinished(WebView view, String url) {
-			super.onPageFinished(view, url);
-			progress.setVisibility(View.GONE);
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		setHasOptionsMenu(true);
+		if (savedInstanceState != null) {
+			url = savedInstanceState.getString(STATE_URL);
 		}
 	}
 
-	class LoadUrlTask extends AsyncTask<Void, Void, Void> implements
-			JsonParseListener {
+	@Override
+	public void onStart() {
+		Log.v(TAG, "onStart");
+		super.onStart();
+		if (url == null) {
+			Log.v(TAG, "Resolving url...");
+			task = new ResolveUrlTask();
+			task.execute();
+		} else {
+			loadUrl();
+		}
+	}
+	
+	class ResolveUrlTask extends AsyncTask<Void, Void, Void> implements JsonParseListener {
 		
-		private String loadedUrl;
-
+		private String resolvedUrl;
+		
 		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
 			progress.setVisibility(View.VISIBLE);
 		}
-
+		
 		@Override
 		protected Void doInBackground(Void... voidRay) {
 			try {
@@ -133,15 +126,15 @@ public class ThingFragment extends Fragment {
 				if (thing == null) {
 					return null;
 				}
-
+		
 				URL url = new URL("http://www.reddit.com/by_id/" + thing.name
 						+ ".json");
 				Log.v(TAG, url.toString());
-
+		
 				HttpURLConnection connection = (HttpURLConnection) url
 						.openConnection();
 				connection.connect();
-
+		
 				InputStream stream = connection.getInputStream();
 				try {
 					JsonReader reader = new JsonReader(new InputStreamReader(
@@ -157,40 +150,63 @@ public class ThingFragment extends Fragment {
 			}
 			return null;
 		}
-
+		
 		public void onUrl(String url) {
-			loadedUrl = url;
+			resolvedUrl = url;
 		}
-
+		
 		@Override
 		protected void onPostExecute(Void intoTheVoid) {
 			super.onPostExecute(intoTheVoid);
-			if (loadedUrl == null || loadedUrl.isEmpty()) {
+			if (resolvedUrl == null || resolvedUrl.isEmpty()) {
 				Log.v(TAG, "Url is null");
 				return;
 			}
-			url = loadedUrl;
-			webView.loadUrl(url);
-			enableUrlMenuItems(true);
+			url = resolvedUrl;
+			loadUrl();
+		}
+	}
+
+	private void loadUrl() {
+		webView.loadUrl(url);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		webView.onResume();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		webView.onPause();
+	}
+	
+	@Override
+	public void onStop() {
+		Log.v(TAG, "onStop");
+		super.onStop();
+		if (task != null) {
+			task.cancel(true);
+			task = null;
 		}
 	}
 	
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString(STATE_URL, url);
+	}
+
 	@Override
 	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		super.onCreateOptionsMenu(menu, inflater);
 		inflater.inflate(R.menu.thing_fragment, menu);
-		this.menu = menu;
-		enableUrlMenuItems(false);
 	}
-	
-	private void enableUrlMenuItems(boolean enabled) {
-		menu.findItem(R.id.menu_copy_link).setEnabled(enabled);
-		menu.findItem(R.id.menu_view).setEnabled(enabled);
-	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		Log.v(TAG, "onOptionsItemSelected!");
 		super.onOptionsItemSelected(item);
 		
 		switch (item.getItemId()) {
@@ -206,15 +222,18 @@ public class ThingFragment extends Fragment {
 	}
 
 	private void handleCopyLinkItem() {
-		ClipboardManager clip = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
-		clip.setText(url);
-		Toast.makeText(getActivity(), url, Toast.LENGTH_SHORT).show();
+		if (url != null) {
+			ClipboardManager clip = (ClipboardManager) getActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+			clip.setText(url);
+			Toast.makeText(getActivity(), url, Toast.LENGTH_SHORT).show();
+		}
 	}
 	
 	private void handleViewItem() {
-		Intent intent = new Intent(Intent.ACTION_VIEW);
-		intent.setData(Uri.parse(url));
-		startActivity(Intent.createChooser(intent, getString(R.string.menu_view)));
+		if (url != null) {
+			Intent intent = new Intent(Intent.ACTION_VIEW);
+			intent.setData(Uri.parse(url));
+			startActivity(Intent.createChooser(intent, getString(R.string.menu_view)));
+		}
 	}
-	
 }
