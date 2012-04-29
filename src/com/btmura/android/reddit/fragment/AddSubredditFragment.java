86ebbaac2b0941699e1f467957cd8480e5c2ff43
Brian Muramatsu <btmura@gmail.com>
2012-04-29 15:31:15 -0700

    Suggest Subreddits in Dialog

--- src/com/btmura/android/reddit/fragment/AddSubredditFragment.java
+++ src/com/btmura/android/reddit/fragment/AddSubredditFragment.java
@@ -20,25 +20,37 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.LoaderManager.LoaderCallbacks;
 import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.InputFilter;
 import android.text.Spanned;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
-import android.widget.EditText;
 
 import com.btmura.android.reddit.Provider;
 import com.btmura.android.reddit.Provider.Subreddits;
 import com.btmura.android.reddit.R;
+import com.btmura.android.reddit.widget.SubredditAdapter;
 
-public class AddSubredditFragment extends DialogFragment {
+public class AddSubredditFragment extends DialogFragment implements
+        TextWatcher,
+        AdapterView.OnItemClickListener,
+        LoaderCallbacks<Cursor> {
 
     public static final String TAG = "AddSubredditFragment";
+
+    private static final String LOADER_ARG_QUERY = "q";
 
     private static final InputFilter[] INPUT_FILTERS = new InputFilter[] {
         new SubredditInputFilter(),
@@ -49,6 +61,8 @@ public class AddSubredditFragment extends DialogFragment {
     }
 
     private SubredditNameHolder nameHolder;
+    private SubredditAdapter adapter;
+    private AutoCompleteTextView nameField;
 
     public static AddSubredditFragment newInstance() {
         return new AddSubredditFragment();
@@ -61,26 +75,34 @@ public class AddSubredditFragment extends DialogFragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adapter = new SubredditAdapter(getActivity(), null, false);
+    }
+
+    @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View v = inflater.inflate(R.layout.add_subreddit, null, false);
-        final EditText subredditName = (EditText) v.findViewById(R.id.subreddit_name);        
+        nameField = (AutoCompleteTextView) v.findViewById(R.id.subreddit_name);
         final CheckBox addFrontPage = (CheckBox) v.findViewById(R.id.add_front_page);
-        
-        addFrontPage.setOnCheckedChangeListener(new OnCheckedChangeListener() {           
+
+        addFrontPage.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                subredditName.setEnabled(!isChecked);
+                nameField.setEnabled(!isChecked);
             }
         });
-                
+
         String name = nameHolder.getSubredditName();
         int length = name != null ? name.length() : 0;
-        subredditName.setText(name);
-        subredditName.setSelection(length, length);
-        subredditName.setFilters(INPUT_FILTERS);
-                
-        return new AlertDialog.Builder(getActivity()).setView(v)
-                .setTitle(R.string.add_subreddit)
+        nameField.setText(name);
+        nameField.setSelection(length, length);
+        nameField.setFilters(INPUT_FILTERS);
+        nameField.setAdapter(adapter);
+        nameField.addTextChangedListener(this);
+        nameField.setOnItemClickListener(this);
+
+        return new AlertDialog.Builder(getActivity()).setView(v).setTitle(R.string.add_subreddit)
                 .setNegativeButton(android.R.string.cancel, null)
                 .setPositiveButton(R.string.button_add, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
@@ -88,14 +110,46 @@ public class AddSubredditFragment extends DialogFragment {
                         if (addFrontPage.isChecked()) {
                             name = "";
                         } else {
-                            name = subredditName.getText().toString();
+                            name = nameField.getText().toString();
                         }
-                        
-                        ContentValues values = new ContentValues(1);                        
+
+                        ContentValues values = new ContentValues(1);
                         values.put(Subreddits.COLUMN_NAME, name);
                         Provider.addSubredditInBackground(getActivity(), values);
                     }
                 }).create();
+    }
+
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (s.length() >= nameField.getThreshold()) {
+            Bundle args = new Bundle(1);
+            args.putString(LOADER_ARG_QUERY, s.toString());
+            getLoaderManager().restartLoader(0, args, this);
+        }
+    }
+
+    public void afterTextChanged(Editable s) {
+    }
+
+    public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+        String name = adapter.getName(getActivity(), position);
+        nameField.setText(name);
+    }
+
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String query = args.getString(LOADER_ARG_QUERY);
+        return SubredditAdapter.createLoader(getActivity(), query);
+    }
+
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
     }
 
     static class SubredditInputFilter implements InputFilter {
