f212637c859a24fbbb70e373d988fe6fe9b991f0
Brian Muramatsu <btmura@gmail.com>
2012-06-17 17:09:42 -0700

    Move SyncAdapter within SyncAdapterService

--- src/com/btmura/android/reddit/provider/SyncAdapterService.java
+++ src/com/btmura/android/reddit/provider/SyncAdapterService.java
@@ -16,12 +16,30 @@
 
 package com.btmura.android.reddit.provider;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
 import android.app.Service;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.ContentProviderOperation;
+import android.content.Context;
 import android.content.Intent;
+import android.content.OperationApplicationException;
+import android.content.SyncResult;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
 
 import com.btmura.android.reddit.Debug;
+import com.btmura.android.reddit.accounts.AccountAuthenticator;
+import com.btmura.android.reddit.entity.Subreddit;
+import com.btmura.android.reddit.provider.SubredditProvider.Subreddits;
 
 public class SyncAdapterService extends Service {
 
@@ -33,5 +51,66 @@ public class SyncAdapterService extends Service {
             Log.d(TAG, "onBind");
         }
         return new SyncAdapter(this).getSyncAdapterBinder();
+    }
+
+    static class SyncAdapter extends AbstractThreadedSyncAdapter {
+
+        public SyncAdapter(Context context) {
+            super(context, true);
+        }
+
+        @Override
+        public void onPerformSync(Account account, Bundle extras, String authority,
+                ContentProviderClient provider, SyncResult syncResult) {
+            if (Debug.DEBUG_SYNC) {
+                Log.d(TAG, "onPerformSync");
+            }
+
+            AccountManager manager = AccountManager.get(getContext());
+
+            try {
+                String cookie = manager.blockingGetAuthToken(account,
+                        AccountAuthenticator.AUTH_TOKEN_COOKIE,
+                        true);
+
+                ArrayList<Subreddit> subreddits = NetApi.query(cookie);
+                int count = subreddits.size();
+
+                ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>(count);
+                for (int i = 0; i < count; i++) {
+                    ops.add(ContentProviderOperation.newInsert(Subreddits.CONTENT_URI)
+                            .withValue(Subreddits.COLUMN_ACCOUNT, account.name)
+                            .withValue(Subreddits.COLUMN_NAME, subreddits.get(i).name)
+                            .build());
+                }
+                provider.applyBatch(ops);
+
+                if (Debug.DEBUG_SYNC) {
+                    Log.d(TAG, "Synced " + count + " subreddits.");
+                }
+
+
+            } catch (OperationCanceledException e) {
+                Log.e(TAG, "onPerformSync", e);
+                syncResult.stats.numAuthExceptions++;
+
+            } catch (AuthenticatorException e) {
+                Log.e(TAG, "onPerformSync", e);
+                syncResult.stats.numAuthExceptions++;
+
+            } catch (IOException e) {
+                Log.e(TAG, "onPerformSync", e);
+                syncResult.stats.numIoExceptions++;
+
+            } catch (RemoteException e) {
+                Log.e(TAG, "onPerformSync", e);
+                syncResult.databaseError = true;
+
+            } catch (OperationApplicationException e) {
+                Log.e(TAG, "onPerformSync", e);
+                syncResult.databaseError = true;
+
+            }
+        }
     }
 }
