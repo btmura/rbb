4edf6e4bb82a896bc97ad903e2b2beadc2f096af
Brian Muramatsu <btmura@gmail.com>
2012-06-29 15:20:55 -0700

    Copy animations from AbstractBrowserActivity

--- src/com/btmura/android/reddit/activity/LoginBrowserActivity.java
+++ src/com/btmura/android/reddit/activity/LoginBrowserActivity.java
@@ -16,10 +16,15 @@
 
 package com.btmura.android.reddit.activity;
 
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.ActionBar;
-import android.app.FragmentManager;
 import android.app.ActionBar.OnNavigationListener;
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.app.FragmentManager.OnBackStackChangedListener;
 import android.app.FragmentTransaction;
 import android.app.LoaderManager.LoaderCallbacks;
@@ -36,6 +41,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.btmura.android.reddit.Debug;
 import com.btmura.android.reddit.LoaderIds;
@@ -68,6 +74,16 @@ public class LoginBrowserActivity extends Activity implements
 
     public static final String TAG = "LoginBrowserActivity";
 
+    private static final int ANIMATION_OPEN_NAV = 0;
+    private static final int ANIMATION_CLOSE_NAV = 1;
+    private static final int ANIMATION_OPEN_SIDE_NAV = 2;
+    private static final int ANIMATION_CLOSE_SIDE_NAV = 3;
+    private static final int ANIMATION_EXPAND_SUBREDDIT_NAV = 4;
+    private static final int ANIMATION_EXPAND_THING_NAV = 5;
+
+    private static final int NAV_LAYOUT_ORIGINAL = 0;
+    private static final int NAV_LAYOUT_SIDENAV = 1;
+
     private ActionBar bar;
     private AccountSpinnerAdapter adapter;
 
@@ -75,8 +91,23 @@ public class LoginBrowserActivity extends Activity implements
     private int slfFlags;
     private int tlfFlags;
     private ViewPager thingPager;
+
     private View navContainer;
+    private View subredditListContainer;
+    private int subredditListWidth;
     private int thingBodyWidth;
+
+    private View thingClickAbsorber;
+    private int fullNavWidth;
+    private int sideNavWidth;
+    private int duration;
+
+    private AnimatorSet openNavAnimator;
+    private AnimatorSet closeNavAnimator;
+    private AnimatorSet openSideNavAnimator;
+    private AnimatorSet closeSideNavAnimator;
+    private AnimatorSet expandSubredditNavAnimator;
+    private AnimatorSet expandThingNavAnimator;
 
     private SharedPreferences prefs;
 
@@ -121,15 +152,38 @@ public class LoginBrowserActivity extends Activity implements
             thingPager.setOnPageChangeListener(this);
 
             navContainer = findViewById(R.id.nav_container);
+            subredditListContainer = findViewById(R.id.subreddit_list_container);
 
             Resources r = getResources();
             DisplayMetrics dm = r.getDisplayMetrics();
+            subredditListWidth = r.getDimensionPixelSize(R.dimen.subreddit_list_width);
             int padding = r.getDimensionPixelSize(R.dimen.element_padding);
-            int subredditListWidth = r.getDimensionPixelSize(R.dimen.subreddit_list_width);
             if (navContainer != null) {
                 thingBodyWidth = dm.widthPixels / 2 - padding * 4;
             } else {
                 thingBodyWidth = dm.widthPixels / 2 - padding * 3 - subredditListWidth;
+            }
+
+            if (navContainer != null) {
+                thingClickAbsorber = findViewById(R.id.thing_click_absorber);
+                if (thingClickAbsorber != null) {
+                    thingClickAbsorber.setOnClickListener(new OnClickListener() {
+                        public void onClick(View v) {
+                            runAnimation(ANIMATION_CLOSE_SIDE_NAV, null);
+                        }
+                    });
+                }
+
+                fullNavWidth = dm.widthPixels;
+                sideNavWidth = fullNavWidth / 2;
+                duration = r.getInteger(android.R.integer.config_shortAnimTime);
+
+                openNavAnimator = createOpenNavAnimator();
+                closeNavAnimator = createCloseNavAnimator();
+                openSideNavAnimator = createSideNavAnimator(true);
+                closeSideNavAnimator = createSideNavAnimator(false);
+                expandSubredditNavAnimator = createExpandNavAnimator(true);
+                expandThingNavAnimator = createExpandNavAnimator(false);
             }
 
             getFragmentManager().addOnBackStackChangedListener(this);
@@ -290,16 +344,34 @@ public class LoginBrowserActivity extends Activity implements
     private void refreshViews(Thing thing) {
         boolean hasThing = thing != null;
         bar.setDisplayHomeAsUpEnabled(hasThing);
-        thingPager.setVisibility(hasThing ? View.VISIBLE : View.GONE);
-        if (!hasThing) {
-            // Avoid nested executePendingTransactions that would occur by
-            // doing popBackStack. This is a hack to get around stale
-            // adapter issues with the ViewPager after orientation changes.
-            thingPager.post(new Runnable() {
-                public void run() {
-                    refreshThingPager(null);
+        if (navContainer != null) {
+            int currVisibility = navContainer.getVisibility();
+            int nextVisibility = !hasThing ? View.VISIBLE : View.GONE;
+            if (currVisibility != nextVisibility) {
+                if (nextVisibility == View.VISIBLE) {
+                    runAnimation(ANIMATION_OPEN_NAV, null);
+                } else {
+                    runAnimation(ANIMATION_CLOSE_NAV, null);
                 }
-            });
+            } else if (isSideNavShowing()) {
+                if (hasSubredditList()) {
+                    runAnimation(ANIMATION_EXPAND_SUBREDDIT_NAV, null);
+                } else {
+                    runAnimation(ANIMATION_EXPAND_THING_NAV, null);
+                }
+            }
+        } else {
+            thingPager.setVisibility(hasThing ? View.VISIBLE : View.GONE);
+            if (!hasThing) {
+                // Avoid nested executePendingTransactions that would occur by
+                // doing popBackStack. This is a hack to get around stale
+                // adapter issues with the ViewPager after orientation changes.
+                thingPager.post(new Runnable() {
+                    public void run() {
+                        refreshThingPager(null);
+                    }
+                });
+            }
         }
     }
 
@@ -364,11 +436,22 @@ public class LoginBrowserActivity extends Activity implements
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                getFragmentManager().popBackStack();
+                handleHome();
                 return true;
 
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void handleHome() {
+        FragmentManager fm = getFragmentManager();
+        if (fm.getBackStackEntryCount() > 0) {
+            if (navContainer != null && !isSideNavShowing()) {
+                runAnimation(ANIMATION_OPEN_SIDE_NAV, null);
+            } else {
+                fm.popBackStack();
+            }
         }
     }
 
@@ -387,5 +470,178 @@ public class LoginBrowserActivity extends Activity implements
 
     private ThingListFragment getThingListFragment() {
         return (ThingListFragment) getFragmentManager().findFragmentByTag(ThingListFragment.TAG);
+    }
+
+    private void runAnimation(int type, AnimatorListener listener) {
+        navContainer.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        navContainer.setVisibility(View.VISIBLE);
+        thingPager.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        thingPager.setVisibility(View.VISIBLE);
+        AnimatorSet as = getAnimator(type);
+        if (listener != null) {
+            as.addListener(listener);
+        }
+        as.start();
+    }
+
+    private AnimatorSet getAnimator(int type) {
+        switch (type) {
+            case ANIMATION_OPEN_NAV:
+                return openNavAnimator;
+            case ANIMATION_CLOSE_NAV:
+                return closeNavAnimator;
+            case ANIMATION_OPEN_SIDE_NAV:
+                return openSideNavAnimator;
+            case ANIMATION_CLOSE_SIDE_NAV:
+                return closeSideNavAnimator;
+            case ANIMATION_EXPAND_SUBREDDIT_NAV:
+                return expandSubredditNavAnimator;
+            case ANIMATION_EXPAND_THING_NAV:
+                return expandThingNavAnimator;
+            default:
+                throw new IllegalArgumentException();
+        }
+    }
+
+    private AnimatorSet createOpenNavAnimator() {
+        ObjectAnimator ncTransX = ObjectAnimator.ofFloat(navContainer, "translationX",
+                -fullNavWidth, 0).setDuration(duration);
+        ObjectAnimator tpTransX = ObjectAnimator.ofFloat(thingPager, "translationX", 0,
+                fullNavWidth).setDuration(duration);
+
+        AnimatorSet as = new AnimatorSet();
+        as.play(ncTransX).with(tpTransX);
+        as.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                changeNavContainerLayout(NAV_LAYOUT_ORIGINAL);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                navContainer.setLayerType(View.LAYER_TYPE_NONE, null);
+                thingPager.setLayerType(View.LAYER_TYPE_NONE, null);
+                thingPager.setVisibility(View.GONE);
+                refreshThingPager(null);
+            }
+        });
+        return as;
+    }
+
+    private AnimatorSet createCloseNavAnimator() {
+        ObjectAnimator ncTransX = ObjectAnimator.ofFloat(navContainer, "translationX", 0,
+                -subredditListWidth).setDuration(duration);
+
+        AnimatorSet as = new AnimatorSet();
+        as.play(ncTransX);
+        as.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                thingPager.setVisibility(View.GONE);
+                thingPager.setTranslationX(0);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                navContainer.setLayerType(View.LAYER_TYPE_NONE, null);
+                navContainer.setVisibility(View.GONE);
+                thingPager.setLayerType(View.LAYER_TYPE_NONE, null);
+                thingPager.setVisibility(View.VISIBLE);
+            }
+        });
+        return as;
+    }
+
+    private AnimatorSet createSideNavAnimator(final boolean open) {
+        ObjectAnimator ncTransX;
+        ObjectAnimator tpTransX;
+        if (open) {
+            ncTransX = ObjectAnimator.ofFloat(navContainer, "translationX", -sideNavWidth, 0);
+            tpTransX = ObjectAnimator.ofFloat(thingPager, "translationX", 0, sideNavWidth);
+        } else {
+            ncTransX = ObjectAnimator.ofFloat(navContainer, "translationX", 0, -sideNavWidth);
+            tpTransX = ObjectAnimator.ofFloat(thingPager, "translationX", sideNavWidth, 0);
+        }
+        ncTransX.setDuration(duration);
+        tpTransX.setDuration(duration);
+
+        AnimatorSet as = new AnimatorSet();
+        as.play(ncTransX).with(tpTransX);
+        as.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                changeNavContainerLayout(NAV_LAYOUT_SIDENAV);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                navContainer.setLayerType(View.LAYER_TYPE_NONE, null);
+                thingPager.setLayerType(View.LAYER_TYPE_NONE, null);
+                if (!open) {
+                    navContainer.setVisibility(View.GONE);
+                }
+            }
+        });
+        return as;
+    }
+
+    private AnimatorSet createExpandNavAnimator(boolean showSubreddits) {
+        ObjectAnimator ncTransX = ObjectAnimator.ofFloat(navContainer, "translationX", 0,
+                subredditListWidth);
+        ObjectAnimator tpTransX = ObjectAnimator.ofFloat(thingPager, "translationX", sideNavWidth,
+                fullNavWidth);
+        ncTransX.setDuration(duration);
+        tpTransX.setDuration(duration);
+
+        AnimatorSet as = new AnimatorSet();
+        if (showSubreddits) {
+            as.play(ncTransX).with(tpTransX);
+        } else {
+            as.play(tpTransX);
+        }
+
+        as.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                navContainer.setLayerType(View.LAYER_TYPE_NONE, null);
+                changeNavContainerLayout(NAV_LAYOUT_ORIGINAL);
+                navContainer.setTranslationX(0);
+
+                thingPager.setLayerType(View.LAYER_TYPE_NONE, null);
+                thingPager.setVisibility(View.GONE);
+                refreshThingPager(null);
+            }
+        });
+        return as;
+    }
+
+    private boolean isSideNavShowing() {
+        return thingClickAbsorber != null && thingClickAbsorber.isShown();
+    }
+
+    private void changeNavContainerLayout(int layout) {
+        int subredditListVisibility;
+        int clickAbsorberVisibility;
+        switch (layout) {
+            case NAV_LAYOUT_ORIGINAL:
+                subredditListVisibility = hasSubredditList() ? View.VISIBLE : View.GONE;
+                clickAbsorberVisibility = View.GONE;
+                break;
+
+            case NAV_LAYOUT_SIDENAV:
+                subredditListVisibility = View.GONE;
+                clickAbsorberVisibility = View.VISIBLE;
+                break;
+
+            default:
+                throw new IllegalStateException();
+        }
+
+        subredditListContainer.setVisibility(subredditListVisibility);
+        thingClickAbsorber.setVisibility(clickAbsorberVisibility);
+    }
+
+    private boolean hasSubredditList() {
+        return true;
     }
 }
