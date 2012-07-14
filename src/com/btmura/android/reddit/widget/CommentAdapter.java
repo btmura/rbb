39bbf0ea9555d4367387327f957b7034bc6fc6ab
Brian Muramatsu <btmura@gmail.com>
2012-07-13 21:25:05 -0700

    Empty custom view for comments

--- src/com/btmura/android/reddit/widget/CommentAdapter.java
+++ src/com/btmura/android/reddit/widget/CommentAdapter.java
@@ -20,23 +20,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
-import android.text.method.LinkMovementMethod;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.TextView;
 
-import com.btmura.android.reddit.R;
 import com.btmura.android.reddit.entity.Comment;
 
 public class CommentAdapter extends BaseAdapter {
 
     private final ArrayList<Comment> items = new ArrayList<Comment>();
-    private final LayoutInflater inflater;
+    private final Context context;
 
     public CommentAdapter(Context context) {
-        this.inflater = LayoutInflater.from(context);
+        this.context = context;
     }
 
     public void swapData(List<Comment> newItems) {
@@ -72,89 +68,12 @@ public class CommentAdapter extends BaseAdapter {
         return 2;
     }
 
-    @Override
-    public boolean isEnabled(int position) {
-        return false;
-    }
-
     public View getView(int position, View convertView, ViewGroup parent) {
-        View v = convertView;
+        CommentView v = (CommentView) convertView;
         if (v == null) {
-            v = createView(position, parent);
+            v = new CommentView(context);
         }
-        setView(position, v);
+        v.setComment(getItem(position));
         return v;
-    }
-
-    private View createView(int position, ViewGroup parent) {
-        switch (getItemViewType(position)) {
-            case Comment.TYPE_HEADER:
-                return makeView(R.layout.comment_header_row, parent);
-
-            case Comment.TYPE_COMMENT:
-                return makeView(R.layout.comment_row, parent);
-
-            default:
-                throw new IllegalArgumentException("Unexpected view type: "
-                        + getItemViewType(position));
-        }
-    }
-
-    private View makeView(int layout, ViewGroup parent) {
-        View v = inflater.inflate(layout, parent, false);
-        v.setTag(createViewHolder(v));
-        return v;
-    }
-
-    private static ViewHolder createViewHolder(View v) {
-        ViewHolder holder = new ViewHolder();
-        holder.title = (TextView) v.findViewById(R.id.title);
-        holder.body = (TextView) v.findViewById(R.id.body);
-        holder.status = (TextView) v.findViewById(R.id.status);
-        return holder;
-    }
-
-    static class ViewHolder {
-        TextView title;
-        TextView body;
-        TextView status;
-    }
-
-    private void setView(int position, View v) {
-        Comment c = getItem(position);
-        ViewHolder h = (ViewHolder) v.getTag();
-        switch (c.type) {
-            case Comment.TYPE_HEADER:
-                setHeader(h, c);
-                break;
-
-            case Comment.TYPE_COMMENT:
-                setComment(h, c);
-                break;
-
-            default:
-                throw new IllegalArgumentException("Unsupported view type: " + c.type);
-        }
-    }
-
-    private void setHeader(ViewHolder h, Comment c) {
-        h.title.setText(c.title);
-        h.body.setMovementMethod(LinkMovementMethod.getInstance());
-        h.body.setVisibility(c.body != null && c.body.length() > 0 ? View.VISIBLE : View.GONE);
-        h.body.setText(c.body);
-        h.status.setText(c.status);
-    }
-
-    private void setComment(ViewHolder h, Comment c) {
-        h.body.setMovementMethod(LinkMovementMethod.getInstance());
-        h.body.setText(c.body);
-        h.status.setText(c.status);
-        setPadding(h.body, c.nesting);
-        setPadding(h.status, c.nesting);
-    }
-
-    private static void setPadding(View v, int nesting) {
-        v.setPadding(v.getPaddingRight() + nesting * 20, v.getPaddingTop(), v.getPaddingRight(),
-                v.getPaddingBottom());
     }
 }
