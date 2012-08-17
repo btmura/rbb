c5f9edcea9be7e5e4fde95b103501d985a7a085d
Brian Muramatsu <btmura@gmail.com>
2012-08-16 19:29:35 -0700

    Show accounts as preference headers

--- src/com/btmura/android/reddit/activity/SettingsActivity.java
+++ src/com/btmura/android/reddit/activity/SettingsActivity.java
@@ -18,14 +18,84 @@ package com.btmura.android.reddit.activity;
 
 import java.util.List;
 
-import com.btmura.android.reddit.R;
-
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Intent;
+import android.content.Loader;
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class SettingsActivity extends PreferenceActivity {
+import com.btmura.android.reddit.R;
+import com.btmura.android.reddit.content.AccountLoader;
+import com.btmura.android.reddit.content.AccountLoader.AccountResult;
+import com.btmura.android.reddit.provider.SubredditProvider;
+
+public class SettingsActivity extends PreferenceActivity implements LoaderCallbacks<AccountResult> {
+
+    private static final String[] AUTHORITIES = {
+            SubredditProvider.AUTHORITY,
+    };
+
+    private AccountResult result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    public Loader<AccountResult> onCreateLoader(int id, Bundle args) {
+        return new AccountLoader(this, false);
+    }
+
+    public void onLoadFinished(Loader<AccountResult> loader, AccountResult result) {
+        this.result = result;
+        invalidateHeaders();
+    }
+
+    public void onLoaderReset(Loader<AccountResult> loader) {
+        this.result = null;
+        invalidateHeaders();
+    }
 
     @Override
     public void onBuildHeaders(List<Header> target) {
         loadHeadersFromResource(R.xml.settings_headers, target);
+        if (result != null) {
+            String[] accountNames = result.accountNames;
+            int length = accountNames.length;
+            for (int i = 0; i < length; i++) {
+                Header header = new Header();
+                header.title = accountNames[i];
+                target.add(header);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.account_manager_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_add_account:
+                handleAddAccount();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void handleAddAccount() {
+        Intent intent = new Intent(Settings.ACTION_ADD_ACCOUNT);
+        intent.putExtra(Settings.EXTRA_AUTHORITIES, AUTHORITIES);
+        startActivity(intent);
     }
 }
