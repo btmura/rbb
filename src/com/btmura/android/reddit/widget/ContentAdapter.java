fd09f55f935eb3efba873a0cbae5a410f6267b97
Brian Muramatsu <btmura@gmail.com>
2012-11-18 11:26:22 -0800

    Make content browser content more readable

--- src/com/btmura/android/reddit/widget/ContentAdapter.java
+++ src/com/btmura/android/reddit/widget/ContentAdapter.java
@@ -18,6 +18,9 @@ package com.btmura.android.reddit.widget;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -35,18 +38,37 @@ public class ContentAdapter extends BaseCursorAdapter {
     }
 
     @Override
+    public boolean isEnabled(int position) {
+        return false;
+    }
+
+    @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         return inflater.inflate(R.layout.content_row, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        StringBuilder row = new StringBuilder();
+        SpannableStringBuilder row = (SpannableStringBuilder) view.getTag();
+        if (row == null) {
+            row = new SpannableStringBuilder();
+            view.setTag(row);
+        }
+        row.clear();
+        row.clearSpans();
+
         int columnCount = cursor.getColumnCount();
         for (int i = 0; i < columnCount; i++) {
-            String columnName = cursor.getColumnName(i);
-            String columnValue = cursor.getString(i);
-            row.append(columnName).append(": ").append(columnValue);
+            // Append the column name and make it bold.
+            row.append(String.format("%16s: ", cursor.getColumnName(i)));
+            row.setSpan(new ForegroundColorSpan(Color.WHITE), row.length() - 18, row.length(), 0);
+
+            // Append the column value and add a newline if it's not the last
+            // one.
+            String value = cursor.getString(i);
+            if (value != null) {
+                row.append(value);
+            }
             if (i + 1 < columnCount) {
                 row.append("\n");
             }
