9f2e00cbb5b83e90e44b390fcbe25aa5b6244bca
Brian Muramatsu <btmura@gmail.com>
2012-11-24 21:30:16 -0800

    Fix #101: show view profile for one item only

--- src/com/btmura/android/reddit/app/ThingListFragment.java
+++ src/com/btmura/android/reddit/app/ThingListFragment.java
@@ -279,6 +279,8 @@ public class ThingListFragment extends ListFragment implements
     public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
         int count = getListView().getCheckedItemCount();
         mode.setTitle(getResources().getQuantityString(R.plurals.things, count, count));
+        menu.findItem(R.id.menu_view_profile).setVisible(count == 1);
+        menu.findItem(R.id.menu_copy_url).setVisible(count == 1);
         return true;
     }
 
