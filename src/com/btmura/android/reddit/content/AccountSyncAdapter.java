347a52c6016f68f0d17a860826d8baf6f4d5c845
Brian Muramatsu <btmura@gmail.com>
2012-12-20 16:14:30 -0800

    Issue #96: add sync adapter for account info

---
+++ src/com/btmura/android/reddit/content/AccountSyncAdapter.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2012 Brian Muramatsu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.btmura.android.reddit.content;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SyncResult;
+import android.os.Bundle;
+import android.os.IBinder;
+
+/**
+ * {@link AbstractThreadedSyncAdapter} for periodically syncing account
+ * information using the /api/me API method to check for mail and other info.
+ */
+public class AccountSyncAdapter extends AbstractThreadedSyncAdapter {
+
+    public static final String TAG = "AccountSyncAdapter";
+
+    public static class Service extends android.app.Service {
+        @Override
+        public IBinder onBind(Intent intent) {
+            return new AccountSyncAdapter(this).getSyncAdapterBinder();
+        }
+    }
+
+    public AccountSyncAdapter(Context context) {
+        super(context, true);
+    }
+
+    @Override
+    public void onPerformSync(Account account, Bundle extras, String authority,
+            ContentProviderClient provider, SyncResult syncResult) {
+    }
+}
