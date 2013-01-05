501b4353fe3b9994e3a74da9801093a6cb739ba8
Brian Muramatsu <btmura@gmail.com>
2013-01-04 21:47:33 -0800

    Add subreddit search results uri

--- src/com/btmura/android/reddit/app/ContentUriListFragment.java
+++ src/com/btmura/android/reddit/app/ContentUriListFragment.java
@@ -60,6 +60,7 @@ public class ContentUriListFragment extends ListFragment {
         adapter.add(SubredditProvider.SUBREDDITS_URI);
         adapter.add(ThingProvider.THINGS_URI);
         adapter.add(ThingProvider.MESSAGES_URI);
+        adapter.add(ThingProvider.SUBREDDITS_URI);
         adapter.add(ThingProvider.COMMENT_ACTIONS_URI);
         adapter.add(ThingProvider.MESSAGE_ACTIONS_URI);
         adapter.add(ThingProvider.READ_ACTIONS_URI);
