8d893033b0624f17376a15a22e3ba2cb2d1e4a06
Brian Muramatsu <btmura@gmail.com>
2013-09-22 23:09:17 -0700

    Don't show upvoted comments in liked section

--- src/com/btmura/android/reddit/app/ThingTableActionModeController.java
+++ src/com/btmura/android/reddit/app/ThingTableActionModeController.java
@@ -299,7 +299,8 @@ class ThingTableActionModeController implements ThingActionModeController, Thing
 
     @Override
     public void vote(int position, int action) {
-        ThingBundle thingBundle = getThingBundle(position);
+        boolean isLink = getKind(position) == Kinds.KIND_LINK;
+        ThingBundle thingBundle = isLink ? getThingBundle(position) : null;
         Provider.voteAsync(context, accountName, action, getThingId(position), thingBundle);
     }
 
