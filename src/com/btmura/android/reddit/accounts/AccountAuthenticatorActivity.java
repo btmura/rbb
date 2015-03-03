8905f400cf227d03e1675b2e713154d87437c263
Brian Muramatsu <btmura@gmail.com>
2015-03-02 23:31:51 -0800

    Restore fragment for adding accounts

--- src/com/btmura/android/reddit/accounts/AccountAuthenticatorActivity.java
+++ src/com/btmura/android/reddit/accounts/AccountAuthenticatorActivity.java
@@ -21,6 +21,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 
 import com.btmura.android.reddit.R;
+import com.btmura.android.reddit.app.AddAccountFragment;
 import com.btmura.android.reddit.app.AddAccountFragment.OnAccountAddedListener;
 import com.btmura.android.reddit.app.OAuthFragment;
 import com.btmura.android.reddit.content.ThemePrefs;
@@ -33,12 +34,11 @@ public class AccountAuthenticatorActivity extends SupportAccountAuthenticatorAct
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTheme(ThemePrefs.getDialogWhenLargeTheme(this));
+        setTheme(ThemePrefs.getDialogTheme(this));
         setContentView(R.layout.account_authenticator);
 
         if (savedInstanceState == null) {
-            // Fragment frag = AddAccountFragment.newInstance(getIntent().getStringExtra(EXTRA_LOGIN));
-            Fragment frag = OAuthFragment.newInstance(getIntent().getStringExtra(EXTRA_LOGIN));
+            Fragment frag = AddAccountFragment.newInstance(getIntent().getStringExtra(EXTRA_LOGIN));
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             ft.replace(R.id.account_authenticator_container, frag);
             ft.commit();
