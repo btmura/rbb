bda643d695b4836867a489c1e0c960cbc2dfd58e
Brian Muramatsu <btmura@gmail.com>
2015-05-30 23:26:14 -0700

    Fix broken test to use https urls

--- tests/src/com/btmura/android/reddit/text/MarkdownFormatter_NamedLinksTest.java
+++ tests/src/com/btmura/android/reddit/text/MarkdownFormatter_NamedLinksTest.java
@@ -37,42 +37,42 @@ public class MarkdownFormatter_NamedLinksTest extends AbstractFormatterTest {
 
     public void testFormat_nestedBrackets() {
         CharSequence cs = assertNamedLinksFormat("[[link]](/abc)", "[link]");
-        assertUrlSpan(cs, 0, 6, "http://www.reddit.com/abc");
+        assertUrlSpan(cs, 0, 6, "https://www.reddit.com/abc");
     }
 
     public void testFormat_nestedBrackets2() {
         CharSequence cs = assertNamedLinksFormat("[[a]](/a) [[b]](/b)", "[a] [b]");
-        assertUrlSpan(cs, 0, 3, "http://www.reddit.com/a");
-        assertUrlSpan(cs, 4, 7, "http://www.reddit.com/b");
+        assertUrlSpan(cs, 0, 3, "https://www.reddit.com/a");
+        assertUrlSpan(cs, 4, 7, "https://www.reddit.com/b");
     }
 
     public void testFormat_nestedBrackets3() {
         CharSequence cs = assertNamedLinksFormat("[[a](/a)]", "[a]");
-        assertUrlSpan(cs, 0, 3, "http://www.reddit.com/a");
+        assertUrlSpan(cs, 0, 3, "https://www.reddit.com/a");
     }
 
     public void testFormat_nestedParens() {
         CharSequence cs = assertNamedLinksFormat("Here is a [link](/abc (123) (456)).",
                 "Here is a link.");
-        assertUrlSpan(cs, 10, 14, "http://www.reddit.com/abc");
+        assertUrlSpan(cs, 10, 14, "https://www.reddit.com/abc");
     }
 
     public void testFormat_nestedParens2() {
         CharSequence cs = assertNamedLinksFormat("[abc (def)](/abc (123) (456))", "abc (def)");
-        assertUrlSpan(cs, 0, 9, "http://www.reddit.com/abc");
+        assertUrlSpan(cs, 0, 9, "https://www.reddit.com/abc");
     }
 
     public void testFormat_multiple() {
         CharSequence cs = assertNamedLinksFormat("[Link 1](/a (123)) and [Link 2](/b (456))",
                 "Link 1 and Link 2");
-        assertUrlSpan(cs, 0, 6, "http://www.reddit.com/a");
-        assertUrlSpan(cs, 11, 17, "http://www.reddit.com/b");
+        assertUrlSpan(cs, 0, 6, "https://www.reddit.com/a");
+        assertUrlSpan(cs, 11, 17, "https://www.reddit.com/b");
     }
 
     public void testFormat_nestedParensMultipleLines() {
         CharSequence cs = assertNamedLinksFormat("[Link 1](/a (123))\n[Link 2](/b (456))",
                 "Link 1\nLink 2");
-        assertUrlSpan(cs, 0, 6, "http://www.reddit.com/a");
-        assertUrlSpan(cs, 7, 13, "http://www.reddit.com/b");
+        assertUrlSpan(cs, 0, 6, "https://www.reddit.com/a");
+        assertUrlSpan(cs, 7, 13, "https://www.reddit.com/b");
     }
 }
